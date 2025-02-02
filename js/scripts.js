67e5838ad3d47423a8286c0a7d67c45ac8bd18d8
alighieriadamo <alighieri.adamo@gmail.com>
2025-02-02 18:46:02 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// HEADER PÁGINAS
+document.addEventListener("DOMContentLoaded", function() {
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header-placeholder').innerHTML = data;
+    });
+});

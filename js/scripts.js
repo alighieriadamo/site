efb8464ad79392dee58cc9c5d2d1bc131cfb69f6
alighieriadamo <alighieri.adamo@gmail.com>
2025-02-11 22:00:32 -0300

    Alterações diversas

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 // HEADER PÁGINAS
-document.addEventListener("DOMContentLoaded", function() {
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header-placeholder').innerHTML = data;
-    });
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("header.html")
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById("header-placeholder").innerHTML = data;
+        });
+});
+
+// FOOTER PÁGINAS
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("footer.html")
+        .then((response) => response.text())
+        .then((data) => {
+            document.getElementById("footer-placeholder").innerHTML = data;
+        });
 });
